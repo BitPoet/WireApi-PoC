5afccbdbdf592d8e08eb5e7e7905ed225f3cdc2c
Chris <the.bitpoet@gmail.com>
2021-02-24 15:20:18 +0100

    Add backend and roles constraint

---
+++ api-template.php
@@ -0,0 +1,14 @@
+<?php namespace ProcessWire;
+
+// -------------------------------------------------------
+// You can add your routes here
+// -------------------------------------------------------
+
+
+
+// -------------------------------------------------------
+
+// Apply routes and handle the request. Return 404 if no
+// route matches.
+
+$api->handleRequest();
